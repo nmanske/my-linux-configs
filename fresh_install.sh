3658b558c6521611b1723cce4c499d7454b119a9
nmanske <nathan.manske@utexas.edu>
2018-09-15 18:49:20 -0500

    Useful tools from cli improved blog post

--- fresh_install.sh
+++ fresh_install.sh
@@ -20,7 +20,7 @@ FUN='cowsay fortune cmatrix sl hollywood tty-clock toilet oneko nyancat ddate ri
 PIP_PACKAGES='platformio glances'
 
 # Install using another method (check the website)
-OTHER_PACKAGES='Other Packages: amdgpu-pro atom libreoffice gimp pia bash-snippets tldr vtop neofetch lolcat krita'
+OTHER_PACKAGES='Other Packages: amdgpu-pro atom libreoffice gimp pia bash-snippets tldr vtop neofetch lolcat krita bat prettyping fzf diff-so-fancy fd ncdu ack ag jq entr'
 ATOM_PACKAGES='Atom Packages: platformio-ide-{debugger,terminal} minimap-{,cursorline,find-and-replace,highlight-selected} open-recent'
 VSCODE_PACKAGES='Visual Studio Code Packages: platformio-ide seti-icons settings-sync c-cpp-intellisense output-colorizer native-debug seti-monokai-theme'
 FIREFOX_PLUGINS='Firefox Plugins: uBlock decentraleyes privacy-settings self-destructing-cookies noscript canvas-blocker tab-toggle toolkit-for-ynab multiple-tab-handler pocket bitwarden flagfox text-contrast-for-dark-themes ubuntu-modifications'
